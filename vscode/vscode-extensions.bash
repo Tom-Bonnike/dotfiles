1f27477014771e1f97f1eade3f30163f44ed3b6e
Tom Bonnike <bonniketom@gmail.com>
2018-05-19 11:44:49 +0200

    Add vscode config files

---
+++ vscode/vscode-extensions.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Generated using: code --list-extensions | xargs -L 1 echo code --install-extension
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension Compulim.vscode-ipaddress
+code --install-extension EditorConfig.EditorConfig
+code --install-extension castwide.solargraph
+code --install-extension christian-kohler.path-intellisense
+code --install-extension chrmarti.regex
+code --install-extension cssho.vscode-svgviewer
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension donjayamanne.githistory
+code --install-extension dzannotti.vscode-babel-coloring
+code --install-extension eg2.vscode-npm-script
+code --install-extension esbenp.prettier-vscode
+code --install-extension file-icons.file-icons
+code --install-extension flowtype.flow-for-vscode
+code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension ms-vscode.Go
+code --install-extension ms-vscode.sublime-keybindings
+code --install-extension naumovs.color-highlight
+code --install-extension naumovs.theme-oceanicnext
+code --install-extension pnp.polacode
+code --install-extension rebornix.Ruby
+code --install-extension tombonnike.vscode-status-bar-format-toggle
+code --install-extension wix.vscode-import-cost
