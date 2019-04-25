abbdb5a21adeec71330d64ef2487ea0c8fb9c55d
Tom Bonnike <Tom-Bonnike@users.noreply.github.com>
2019-04-25 14:04:41 +0200

    Update vscode-extensions.bash

--- vscode/vscode-extensions.bash
+++ vscode/vscode-extensions.bash
@@ -2,16 +2,17 @@
 # Generated using: code --list-extensions | xargs -L 1 echo code --install-extension
 code --install-extension apollographql.vscode-apollo
 code --install-extension bierner.markdown-preview-github-styles
-code --install-extension CoenraadS.bracket-pair-colorizer
-code --install-extension Compulim.vscode-ipaddress
-code --install-extension EditorConfig.EditorConfig
 code --install-extension castwide.solargraph
 code --install-extension christian-kohler.path-intellisense
 code --install-extension chrmarti.regex
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension Compulim.vscode-ipaddress
 code --install-extension cssho.vscode-svgviewer
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension donjayamanne.githistory
 code --install-extension dzannotti.vscode-babel-coloring
+code --install-extension eamodio.gitlens
+code --install-extension EditorConfig.EditorConfig
 code --install-extension eg2.vscode-npm-script
 code --install-extension esbenp.prettier-vscode
 code --install-extension file-icons.file-icons
@@ -22,9 +23,5 @@ code --install-extension ms-vscode.Go
 code --install-extension ms-vscode.sublime-keybindings
 code --install-extension naumovs.color-highlight
 code --install-extension naumovs.theme-oceanicnext
-code --install-extension pnp.polacode
-code --install-extension Prisma.vscode-graphql
-code --install-extension rebornix.Ruby
+code --install-extension OfHumanBondage.react-proptypes-intellisense
 code --install-extension tombonnike.vscode-status-bar-format-toggle
-code --install-extension waderyan.gitblame
-code --install-extension wix.vscode-import-cost
