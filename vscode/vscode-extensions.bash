e59ac9c6682b979c18fcb687d18130eb06e8b08b
Tom Bonnike <Tom-Bonnike@users.noreply.github.com>
2019-06-06 14:11:50 +0200

    Update vscode-extensions.bash

--- vscode/vscode-extensions.bash
+++ vscode/vscode-extensions.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Generated using: code --list-extensions | xargs -L 1 echo code --install-extension
-code --install-extension apollographql.vscode-apollo
 code --install-extension bierner.markdown-preview-github-styles
 code --install-extension castwide.solargraph
 code --install-extension christian-kohler.path-intellisense
@@ -18,11 +17,13 @@ code --install-extension file-icons.file-icons
 code --install-extension flowtype.flow-for-vscode
 code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension mgmcdermott.vscode-language-babel
 code --install-extension ms-vscode.Go
 code --install-extension ms-vscode.sublime-keybindings
 code --install-extension naumovs.color-highlight
 code --install-extension naumovs.theme-oceanicnext
 code --install-extension OfHumanBondage.react-proptypes-intellisense
 code --install-extension pnp.polacode
+code --install-extension redhat.java
 code --install-extension tombonnike.vscode-status-bar-format-toggle
 code --install-extension wix.vscode-import-cost
