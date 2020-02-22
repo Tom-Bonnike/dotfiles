5f2b019a0c5a8e0759fe0cd04177e997bd7fbc76
Tom Bonnike <Tom-Bonnike@users.noreply.github.com>
2020-02-22 10:18:04 +0100

    Update vscode-extensions.bash

--- vscode/vscode-extensions.bash
+++ vscode/vscode-extensions.bash
@@ -1,5 +1,5 @@
-#!/bin/bash
 # Generated using: code --list-extensions | xargs -L 1 echo code --install-extension
+code --install-extension ardenivanov.svelte-intellisense
 code --install-extension bierner.markdown-preview-github-styles
 code --install-extension castwide.solargraph
 code --install-extension christian-kohler.path-intellisense
@@ -8,7 +8,6 @@ code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension Compulim.vscode-ipaddress
 code --install-extension cssho.vscode-svgviewer
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension donjayamanne.githistory
 code --install-extension eamodio.gitlens
 code --install-extension EditorConfig.EditorConfig
 code --install-extension eg2.vscode-npm-script
@@ -16,15 +15,13 @@ code --install-extension esbenp.prettier-vscode
 code --install-extension FerrierBenjamin.fold-unfold-all-icone
 code --install-extension file-icons.file-icons
 code --install-extension flowtype.flow-for-vscode
+code --install-extension JamesBirtles.svelte-vscode
 code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension kumar-harsh.graphql-for-vscode
 code --install-extension mgmcdermott.vscode-language-babel
 code --install-extension ms-vscode.Go
 code --install-extension ms-vscode.sublime-keybindings
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension naumovs.color-highlight
 code --install-extension naumovs.theme-oceanicnext
-code --install-extension OfHumanBondage.react-proptypes-intellisense
-code --install-extension pnp.polacode
-code --install-extension redhat.java
 code --install-extension tombonnike.vscode-status-bar-format-toggle
-code --install-extension wix.vscode-import-cost
