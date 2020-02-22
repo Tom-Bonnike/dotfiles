20f7a9a699773419cb18a4d056c58de879828b49
Tom Bonnike <Tom-Bonnike@users.noreply.github.com>
2020-02-22 11:01:11 +0100

    Create brew.bash

---
+++ brew.bash
@@ -0,0 +1,25 @@
+# Generated using: brew list | xargs -L 1 echo brew install
+brew install binwalk
+brew install diff-so-fancy
+brew install exiftool
+brew install gcc
+brew install gdbm
+brew install gmp
+brew install isl
+brew install libmpc
+brew install libyaml
+brew install mpfr
+brew install ngrep
+brew install nvm
+brew install openssl
+brew install p7zip
+brew install pngcheck
+brew install python
+brew install readline
+brew install ruby
+brew install sqlite
+brew install ssdeep
+brew install xz
+
+# Generated using: brew cask list | xargs -L 1 echo brew cask install
+brew cask install ngrok
