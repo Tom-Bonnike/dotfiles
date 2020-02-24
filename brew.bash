e5dea8ee23211eb143eb8d8a6f1a74136f53de8c
Tom Bonnike <Tom-Bonnike@users.noreply.github.com>
2020-02-24 10:59:09 +0100

    Update brew.bash

--- brew.bash
+++ brew.bash
@@ -1,24 +1,13 @@
 # Generated using: brew list | xargs -L 1 echo brew install
-brew install binwalk
 brew install diff-so-fancy
 brew install exiftool
-brew install gcc
 brew install gdbm
-brew install gmp
-brew install isl
-brew install libmpc
-brew install libyaml
-brew install mpfr
-brew install ngrep
+brew install oniguruma
 brew install nvm
 brew install openssl
-brew install p7zip
-brew install pngcheck
 brew install python
 brew install readline
-brew install ruby
 brew install sqlite
-brew install ssdeep
 brew install xz
 
 # Generated using: brew cask list | xargs -L 1 echo brew cask install
